3c65cee1070db8b22f4d920e82c2cb29bc9f3f21
Linner-Thomas <linner.thomas@gmx.net>
2019-11-13 12:44:39 +0100

    Implemented junit test

--- src/test/java/de/thro/inf/prg3/a07/tests/OpenMensaAPITests.java
+++ src/test/java/de/thro/inf/prg3/a07/tests/OpenMensaAPITests.java
@@ -8,26 +8,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class OpenMensaAPITests
 {
-
 	private static final Logger logger = LogManager.getLogger(OpenMensaAPITests.class);
 	private OpenMensaAPI openMensaAPI;
 
 	@BeforeAll
 	void setup()
 	{
-
-		// use this to intercept all requests and output them to the logging facilities
 		HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
 		loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
@@ -47,20 +50,15 @@ class OpenMensaAPITests
 	@Test
 	void testGetMeals() throws IOException
 	{
-		// TODO prepare call
+		Call<Meal[]> call = openMensaAPI.getMeals(229, new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date()));
 
-		// TODO execute the call synchronously
-
-		// TODO unwrap the body
-		List<Meal> meals = null;
+		List<Meal> meals = Arrays.asList(call.execute().body());
 
 		assertNotNull(meals);
+		System.out.println("size: " + meals.size());
 		assertNotEquals(0, meals.size());
 
 		for (Meal m : meals)
-		{
 			logger.info(m.toString());
-		}
 	}
-
 }
